ba37ae659249d6f3abade1acda00516575702e9d
Jhonatan1505 <68017985+Jhonatan1505@users.noreply.github.com>
2022-06-16 15:57:42 -0500

    Agregar archivos de proyecto.

---
+++ XamEFCore/XamEFCore/Models/Artista.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamEFCore.Models
+{
+    public class Artista
+    {
+        public int ArtistaID { get; set; }
+        public String Nombre { get; set; }
+    }
+}
